22a1419a23d265de232374d1c7bd922f300e96ef
Matt Young <mcyoung86@gmail.com>
2019-10-09 21:55:10 -0600

    COLLECTION EMAIL: setup to be able to seen summary of collection of exceptions.

--- app/mailers/exception_handler/exception_mailer.rb
+++ app/mailers/exception_handler/exception_mailer.rb
@@ -11,5 +11,14 @@ module ExceptionHandler
              subject: "ERROR - #{Rails.application.class.parent_name} - (#{@exception.status}) #{@exception.message}"
         Rails.logger.info "Exception Sent To → #{ExceptionHandler.config.email[:to]}"
       end
+
+      def collection_email(frequency, grouped_exceptions = [])
+        @frequency          = frequency.to_s.titleize
+        @grouped_exceptions = grouped_exceptions
+        
+        mail to:      ExceptionHandler.config.email[:to],
+             subject: "#{@frequency} Error Digest - #{Rails.application.class.parent_name}"
+        Rails.logger.info "#{@frequency} Digest Sent To → #{ExceptionHandler.config.email[:to]}"
+      end
   end
 end
